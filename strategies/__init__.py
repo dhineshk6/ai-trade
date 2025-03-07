a4a625e49c17040ed75a88209e532d89f6146a93
Dhinesh Kumar <dhineshk6@gmail.com>
2025-03-06 23:13:42 -0500

    Update __init__.py

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -1,15 +1,19 @@
 """
 Trading Strategies Package
 ------------------------
-Created: 2025-03-07 03:51:58
+Created: 2025-03-07 04:11:59
 Author: dhineshk6
 """
 
-from .trading_strategy import TradingStrategy
-from .risk_manager import RiskManager
-from .backtest_engine import BacktestEngine
+# Import strategy components
+from strategies.trading_strategy import TradingStrategy
+from strategies.risk_manager import RiskManager
+from strategies.backtest_engine import BacktestEngine
+from strategies.strategy_base import StrategyBase
 
+# Export components
 __all__ = [
+    'StrategyBase',
     'TradingStrategy',
     'RiskManager',
     'BacktestEngine'
@@ -18,4 +22,4 @@ __all__ = [
 # Package metadata
 __version__ = '1.0.0'
 __author__ = 'dhineshk6'
-__timestamp__ = '2025-03-07 03:51:58'
+__timestamp__ = '2025-03-07 04:11:59'
